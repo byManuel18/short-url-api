54f1e9400125e297051c8661520dc974eb442113
byManuel18 <antoniomanu533@gmail.com>
2025-03-01 18:50:21 +0100

    add class validators

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import { Environments, EnvKeys } from './config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   const configService = app.get(ConfigService);
   const env = configService.get<string>(EnvKeys.ENV);
+
   if (env === Environments.DEV) {
     const config = new DocumentBuilder()
       .setTitle('API Docs')
@@ -19,6 +29,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
   }
+
   await app.listen(configService.get<number>(EnvKeys.PORT) ?? 3000);
 }
 void bootstrap();
